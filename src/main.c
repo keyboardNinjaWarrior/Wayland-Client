72b9e27bfd54eaefc1e319a482b80dc1dc67709a
Lain <ibnul.aftab@proton.me>
2026-07-22 13:22:55 +0500

    Added Double Buffering
    Major Change:
    Made a frame array of length two. Adjusted the algorithm
    wl_buffer_release, wl_callback_frame_done and xdg_surface_configure to
    accomodate for two buffers using a for loop.
    
    Minor Changes:
    Removed unecessary logs, sepecially in empty functions. The
    wl_registry_listener is moved bellow a few instructions.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,10 @@
  * Email: 	ibnul.aftab@proton.me 	*
  * 					*/
 
-// TODO(Nadeem Anwar): plan error handling
-// TODO(Nadeem Anwar): replace exit with proper function that deallocates memory
-// XXX: DON'T TRY TO MAKE ERROR HANDLING IF STATEMENTS INTO A MACRO. EXTREMELY BAD IDEA. PLEASE BEAR WITH IT.
+// TODO: plan error handling
+// TODO: replace exit with proper function that deallocates memory
+// XXX:  DON'T TRY TO MAKE ERROR HANDLING IF STATEMENTS INTO A MACRO. EXTREMELY BAD IDEA. PLEASE BEAR WITH IT.
+// 	 Strips the abblity to write custom messages.	
 
 #define _GNU_SOURCE
 
@@ -22,8 +23,8 @@
 #include "debug.h"
 #include "render.h"
 
-#define WIDTH 	500
-#define HEIGHT 	600
+#define WIDTH 	800
+#define HEIGHT 	800
 
 #define VAR(x)	#x
 
@@ -31,18 +32,36 @@ enum FLAGS {DEFAULT, CREATE, RESIZE};
 
 static struct frame {	
 	struct wl_buffer * buffer;
-	struct wl_callback * callback;
 	uint32_t * pixels;
+	bool free;
+
+	#ifdef DEBUG
+	uint8_t id;
+	#endif
+
+} frame[2] = {{0}, {0}};
+
+static struct metadata {	
+	struct wl_callback * callback;
 	uint32_t width;
 	uint32_t height;
-	uint32_t size;	
-	uint32_t stride; 
+	uint32_t frame_size;
+	uint32_t stride;
+	uint32_t buffer_size;
 	int fd;
-	bool free;
-} frame = {0};
+	uint8_t * buffer;
+	const uint8_t len;
+} metadata = {
+		.width = 0,
+		.height = 0,
+		.frame_size = 0,
+		.stride = 0,
+		.len = 2,
+		.buffer = NULL
+};
 
-static struct state {	
-	struct wl_display * display;		
+static struct state {
+	struct wl_display * display;
 	struct wl_event_queue * display_queue;
 	struct wl_event_queue * render_queue;
 	struct wl_compositor * compositor;
@@ -51,7 +70,7 @@ static struct state {
 	struct wl_surface * wl_surface;
 	struct xdg_surface * xdg_surface;
 	struct xdg_toplevel * xdg_toplevel;
-	struct frame * frame;
+	
 	enum FLAGS flag;
 	bool running;
 } state = {
@@ -63,14 +82,13 @@ static struct state {
 	.wl_surface = NULL,
 	.xdg_surface = NULL,
 	.xdg_toplevel = NULL,
-	.running = false,
+	
 	.flag = DEFAULT,
-
-	.frame = &frame
+	.running = false,
 };
 
-static void registery_global(void * data, struct wl_registry * registery, uint32_t name, const char * interface, uint32_t version);
-static void registery_global_remove(void * data, struct wl_registry * registery, uint32_t name);
+static void registery_global(void * data, struct wl_registry * registery, uint32_t name, const char * interface, uint32_t version);			// set_globals
+static void registery_global_remove(void * data, struct wl_registry * registery, uint32_t name);							// handle_removed_global
 static void ping (void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial);
 static void xdg_surface_configure (void *data, struct xdg_surface *xdg_surface, uint32_t serial);
 static void xdg_toplevel_close(void *data, struct xdg_toplevel *xdg_toplevel);
@@ -99,6 +117,10 @@ static const struct wl_callback_listener wl_callback_listener = {
 	.done = &wl_callback_frame_done
 };
 
+static const struct wl_buffer_listener wl_buffer_listener = {
+	.release = &wl_buffer_release
+};
+
 int main (void)
 {
 	START_BENCHMARK(1);
@@ -115,28 +137,28 @@ int main (void)
 	
 	state.display_queue = wl_display_create_queue(state.display);
 
+	struct wl_registry * registry = wl_display_get_registry(state.display);
+	struct wl_event_queue * registry_queue = wl_display_create_queue(state.display);
+	wl_proxy_set_queue((struct wl_proxy *) registry, registry_queue);
+
     	const struct wl_registry_listener registery_listener = {
 		.global = &registery_global,
 		.global_remove = &registery_global_remove
 	};
-	
-	struct wl_registry * registry = wl_display_get_registry(state.display);
-	struct wl_event_queue * registry_queue = wl_display_create_queue(state.display);
-	wl_proxy_set_queue((struct wl_proxy *) registry, registry_queue);
 
 	wl_registry_add_listener(registry, &registery_listener, NULL);
 	
 	START_BENCHMARK(2);
 	
 	// Fetches available global objects
-	wl_display_roundtrip_queue(state.display, registry_queue);
+	wl_display_roundtrip_queue(state.display, registry_queue);	
 	
 	END_BENCHMARK(2, BOLD "wl_display_roundtrip_queue()" RESET);
 
-	PRINT_LOG(SUCCESS, "Initialized " BOLD "state.compositor, state.shared_memory, state.window_manager_base" RESET " in " BOLD "wl_display_roundtrip()" RESET);
-
 	wl_registry_destroy(registry);
 	wl_event_queue_destroy(registry_queue);
+	
+	PRINT_LOG(SUCCESS, "Initialized " BOLD "state.compositor, state.shared_memory, state.window_manager_base" RESET " in " BOLD "wl_display_roundtrip()" RESET);
 
 	xdg_wm_base_add_listener(state.window_manager_base, &window_manager_base_listener, NULL);
 
@@ -160,9 +182,9 @@ int main (void)
 
 	PRINT_LOG(SUCCESS, "Initialized " BOLD "state.xdg_surface" RESET " from " BOLD "xdg_wm_base_get_xdg_surface()" RESET);
 
-	state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
-	
 	xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, NULL);
+	
+	state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
 	
 	if(! state.xdg_toplevel)
 	{
@@ -173,7 +195,6 @@ int main (void)
 	PRINT_LOG(SUCCESS, "Initialized " BOLD "state.xdg_toplevel" RESET " from " BOLD "xdg_surface_get_toplevel()" RESET);
 	
 	xdg_toplevel_add_listener(state.xdg_toplevel, &xdg_toplevel_listener, NULL);
-	
 	xdg_toplevel_set_title(state.xdg_toplevel, "Wayland Client");
 
 	wl_surface_commit(state.wl_surface);
@@ -182,10 +203,11 @@ int main (void)
 	
 	state.running = true;
 	while(state.running && wl_display_dispatch_queue(state.display, state.display_queue) != -1);
-
-	wl_buffer_destroy(frame.buffer);
-	wl_callback_destroy(frame.callback);
 	
+	for(int i = 0; i < metadata.len; i++)
+		wl_buffer_destroy(frame[i].buffer);
+
+	wl_callback_destroy(metadata.callback);
 	xdg_toplevel_destroy(state.xdg_toplevel);
 	xdg_surface_destroy(state.xdg_surface);
 	wl_surface_destroy(state.wl_surface);
@@ -195,14 +217,14 @@ int main (void)
 	wl_compositor_destroy(state.compositor);
 
 	wl_display_disconnect(state.display);
-
-	if(munmap(frame.pixels, frame.size) < 0)
+	
+	if(munmap(metadata.buffer, metadata.buffer_size) < 0)
 	{
-		PRINT_LOG(FAIL, "Unable to free memory: " BOLD VAR(frame) RESET " having size = " BOLD "%d" RESET, frame.size);
+		PRINT_LOG(FAIL, "Unable to free memory: " BOLD VAR(metadata.buffer) RESET " having size = " BOLD "%d" RESET, metadata.buffer_size);
 		exit(ERR_MEM);
 	}
-
-	close(frame.fd);
+	
+	close(metadata.fd);
 
 	END_BENCHMARK(1, BOLD "%s()" RESET, __func__);
 	
@@ -210,9 +232,7 @@ int main (void)
 }
 
 static void registery_global(void * data, struct wl_registry * registery, uint32_t name, const char * interface, uint32_t version)
-{
-	START_BENCHMARK(1);
-	
+{	
 	PRINT_LOG(LOG, "Found: " BOLD "%s" RESET, interface);
 	
 	if (! strcmp(interface, wl_compositor_interface.name))
@@ -225,10 +245,8 @@ static void registery_global(void * data, struct wl_registry * registery, uint32
 			exit(ERR_COMPOSITOR);
 		}
 	
-		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.compositor" RESET " from " BOLD "wl_registery_bind()" RESET);
+		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.compositor" RESET " from " BOLD "wl_registery_bind()" RESET, state.compositor);
 		wl_proxy_set_queue((struct wl_proxy *) state.compositor, state.display_queue);
-
-		END_BENCHMARK(1, BOLD "compositor:%s()" RESET, __func__);
 		
 		return;
 	}
@@ -240,16 +258,14 @@ static void registery_global(void * data, struct wl_registry * registery, uint32
 	
 		if(! state.shared_memory)
 		{
-			PRINT_LOG(FAIL, "Unable to initialize " BOLD "state.shared_memory" RESET " from " BOLD "wl_registery_bind()" RESET);
+			PRINT_LOG(FAIL, "Unable to initialize " BOLD "state.shared_memory" RESET " from " BOLD "wl_registery_bind()" RESET, state.shared_memory);
 			exit(ERR_SHM);
 		}
 	
-		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.shared_memory" RESET " from " BOLD "wl_registery_bind()" RESET);
+		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.shared_memory" RESET " from " BOLD "wl_registery_bind()" RESET, state.shared_memory);
 
 		wl_proxy_set_queue((struct wl_proxy *) state.shared_memory, state.display_queue);
-		
-		END_BENCHMARK(1, BOLD "shared_memory:%s()" RESET, __func__);
-		
+
 		return;
 	}
 
@@ -264,44 +280,39 @@ static void registery_global(void * data, struct wl_registry * registery, uint32
 			exit(ERR_XDG_WM_BASE);
 		}
 	
-		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.window_manager_base" RESET " from " BOLD "wl_registery_bind()" RESET);	
+		PRINT_LOG(SUCCESS, "Initialized " BOLD "state.window_manager_base" RESET " from " BOLD "wl_registery_bind()" RESET, state.window_manager_base);	
 		wl_proxy_set_queue((struct wl_proxy *) state.window_manager_base, state.display_queue);
-		
-		
-		END_BENCHMARK(1, BOLD "window_manager:%s()" RESET, __func__);
 		
 		return;
 	}
 }
 
-static void registery_global_remove(void * data, struct wl_registry * registery, uint32_t name) 
-{
-	PRINT_LOG(LOG, "Event dispatched...");	
-}
+static void registery_global_remove(void * data, struct wl_registry * registery, uint32_t name) {}
 
 void ping (void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial)
 {
-	PRINT_LOG(LOG, "Event dispatched...");	
+	PRINT_LOG(LOG, "Application is active!");	
 	
 	xdg_wm_base_pong(xdg_wm_base, serial);
 }
 
-int allocate_shm_file(size_t size)
+inline static int allocate_shm_file()
 {
-	int fd = memfd_create("main", MFD_CLOEXEC);
+	char * const file_name = "frame-buffer";
+	int fd = memfd_create(file_name, MFD_CLOEXEC);
 	
 	if (fd < 0)
 	{
-		PRINT_LOG(FAIL, "Unable to create anonymous file: " BOLD "main" RESET);
+		PRINT_LOG(FAIL, "Unable to create anonymous file: " BOLD "%s" RESET, file_name);
 		
 		exit(ERR_FILE);
 	}
 	
-	PRINT_LOG(SUCCESS, "Created anonymous file: " BOLD "main" RESET);
+	PRINT_LOG(SUCCESS, "Created anonymous file: " BOLD "%s" RESET, file_name);
 	
-	if(ftruncate(fd, size) != 0)
+	if(ftruncate(fd, metadata.buffer_size) != 0)
 	{
-		PRINT_LOG(FAIL, "Unable to truncate file " BOLD  "main" RESET " of size " BOLD "%d" RESET, size);
+		PRINT_LOG(FAIL, "Unable to truncate file " BOLD  "%s" RESET " of size " BOLD "%d" RESET, file_name, metadata.frame_size);
 		close(fd);
 		
 		exit(ERR_FILE);
@@ -310,79 +321,86 @@ int allocate_shm_file(size_t size)
 	return fd;
 }
 
-// TODO(Nadeem Anwar): xdg_toplevel_configure: width == 0 and heingth == 0 not necisserily when the window is created 
+// TODO: xdg_toplevel_configure: width == 0 and heingth == 0 not necisserily when the window is created 
 void xdg_surface_configure (void *data, struct xdg_surface *xdg_surface, uint32_t serial)
 {
 	START_BENCHMARK(1);
-
-	PRINT_LOG(LOG, "Event dispatched...");
 	
 	xdg_surface_ack_configure(state.xdg_surface, serial);
 	
 	switch (state.flag)	
 	{
+		// Add support for double buffering
 		case CREATE:
 			{
-
 				PRINT_LOG(LOG, "Creating frame buffer...");
 			
-				frame.stride = frame.width * 4;
-				frame.size = frame.stride * frame.height;
+				metadata.stride = metadata.width * 4;
+				metadata.frame_size = metadata.stride * metadata.height;
 
-				PRINT_LOG(LOG, "Buffer size: " BOLD "%d" RESET " bytes", frame.size);
-
-				frame.fd = allocate_shm_file(frame.size);
-				frame.pixels = mmap(NULL, frame.size, PROT_READ | PROT_WRITE, MAP_SHARED, frame.fd, 0);
+				PRINT_LOG(LOG, "Buffer size = (%d * %d) * %d = " BOLD "%d" RESET " bytes", 
+					  metadata.width, 4, metadata.height, metadata.frame_size);
 				
-				if(frame.pixels == MAP_FAILED)
+				metadata.buffer_size = metadata.frame_size * metadata.len;
+				
+				PRINT_LOG(LOG, "Total size = %d * %d = " BOLD "%d" RESET " bytes", metadata.frame_size, metadata.len, metadata.buffer_size);
+				
+				metadata.fd = allocate_shm_file();
+
+				// Casting to pointer to uint8_t for pointer arthimatic
+				metadata.buffer = (uint8_t *) mmap(NULL, metadata.buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, metadata.fd, 0);
+				
+				if(metadata.buffer == MAP_FAILED)
 				{
-					PRINT_LOG(FAIL, "Unable to map memory from the annonymous file to frame");
-					close(frame.fd);
+					PRINT_LOG(FAIL, "Unable to map memory from the annonymous file to " BOLD "%s" RESET, VAR(metadata.buffer));
+					close(metadata.fd);
 
 					exit(ERR_MEM);
 				}
 
-				PRINT_LOG(SUCCESS, "Mapped memory from the annonymous file to the frame");
-			
-				memset(frame.pixels, 0x00, frame.size);
+				PRINT_LOG(SUCCESS, "Mapped memory from the annonymous file to " BOLD "%s" RESET, VAR(metadata.buffer));
 
-				struct wl_shm_pool * pool = wl_shm_create_pool(state.shared_memory, frame.fd, frame.size);
-				frame.buffer = wl_shm_pool_create_buffer(pool, 0, frame.width, frame.height, frame.stride, WL_SHM_FORMAT_ARGB8888);
+				struct wl_shm_pool * pool = wl_shm_create_pool(state.shared_memory, metadata.fd, metadata.buffer_size);
+				
+				for(uint8_t i = 0; i < metadata.len; i++)
+				{
+					frame[i].buffer = wl_shm_pool_create_buffer	(pool, 
+											 metadata.frame_size * i, 
+											 metadata.width, 
+											 metadata.height, 
+											 metadata.stride, 
+											 WL_SHM_FORMAT_ARGB8888);
+
+					frame[i].pixels = (uint32_t *) (metadata.buffer + (metadata.frame_size * i));	
+
+					#ifdef DEBUG
+					frame[i].id = i;
+					#endif
+
+					wl_buffer_add_listener(frame[i].buffer, &wl_buffer_listener, &frame[i]);
+					frame[i].free = true;
+				}
+				
+				PRINT_LOG(LOG, "Created frame buffers: %s and %s", VAR(frame[0]), VAR(frame[1]));
+				
 				wl_shm_pool_destroy(pool);
 
-				PRINT_LOG(LOG, "Created frame buffer: state.frame");
-
-				static const struct wl_buffer_listener wl_buffer_listener = {
-					.release = &wl_buffer_release
-			};
-
-				wl_buffer_add_listener(frame.buffer, &wl_buffer_listener, NULL);
-
-				frame.free = true;
-				
-				PRINT_LOG(LOG, "frame.free = %d", frame.free);
-				
-				frame.callback = wl_surface_frame(state.wl_surface);
-				wl_callback_add_listener(frame.callback, &wl_callback_listener, data);
-
-				wl_surface_attach(state.wl_surface, frame.buffer, 0, 0);
-				wl_surface_commit(state.wl_surface);
-
 				state.flag = DEFAULT;
-				
-				END_BENCHMARK(1, "frame creation : " BOLD "%s()" RESET, __func__);
-				
-				return;
+
+				goto xdg_surface_configure_end;
 			}
 
 		default: break;
 	}
-		
-	wl_callback_destroy(frame.callback);
-	frame.callback = wl_surface_frame(state.wl_surface);
-	wl_callback_add_listener(frame.callback, &wl_callback_listener, data);
 	
-	wl_surface_attach(state.wl_surface, frame.buffer, 0, 0);
+	wl_callback_destroy(metadata.callback);
+	
+	xdg_surface_configure_end:
+
+	metadata.callback = wl_surface_frame(state.wl_surface);
+	wl_callback_add_listener(metadata.callback, &wl_callback_listener, data);
+		
+	wl_surface_attach(state.wl_surface, frame[0].buffer, 0, 0);
 	wl_surface_commit(state.wl_surface);
 	
 	END_BENCHMARK(1, "%s()", __func__)
@@ -390,21 +408,23 @@ void xdg_surface_configure (void *data, struct xdg_surface *xdg_surface, uint32_
 
 void xdg_toplevel_configure (void *data, struct xdg_toplevel *xdg_toplevel, int32_t width, int32_t height, struct wl_array *states)
 {
-	PRINT_LOG(LOG, "Event dispatched...");
+
 	PRINT_LOG(LOG, "Width: %d; Height: %d", width, height);
 
 	if(width == 0 && height == 0) 
 	{
-		frame.width = WIDTH;
-		frame.height = HEIGHT;
+		metadata.width = WIDTH;
+		metadata.height = HEIGHT;
 
 		state.flag = CREATE;
+
+		PRINT_LOG(LOG, "No height and width specified!");
 	}
 	else 
 	{
-		frame.width = width;
-		frame.height = height;
-	}
+		metadata.width = width;
+		metadata.height = height;
+	}	
 }
 
 void wl_callback_frame_done(void *data, struct wl_callback *wl_callback, uint32_t callback_data)
@@ -418,47 +438,49 @@ void wl_callback_frame_done(void *data, struct wl_callback *wl_callback, uint32_
 	time = callback_data;
 	#endif
 
-	wl_callback_destroy(frame.callback);
-	frame.callback = wl_surface_frame(state.wl_surface);
-	wl_callback_add_listener(frame.callback, &wl_callback_listener, data);
+	wl_callback_destroy(metadata.callback);
+	metadata.callback = wl_surface_frame(state.wl_surface);
+	wl_callback_add_listener(metadata.callback, &wl_callback_listener, data);
 	
-	if(! frame.free)
-	{	
-		PRINT_LOG(LOG, "frame.free = %d", frame.free);	
-		return;
+	struct frame * free_frame;
+
+	for(int i = 0; i < metadata.len; i++)
+	{
+		if(frame[i].free)
+		{	
+			PRINT_LOG(LOG, "frame[%d].free = %d", frame[i].id, frame[i].free);
+			free_frame = &frame[i];
+			goto wl_callback_frame_done_free;
+		}
 	}
+	
+	return;	
+	
+	wl_callback_frame_done_free:
+	free_frame->free = false;
+	PRINT_LOG(LOG, BOLD "frame[%d]" RESET " chosen for rendering", free_frame->id);
 
-	frame.free = false;
-	PRINT_LOG(LOG, "frame.free = %d", frame.free);
+	draw(free_frame->pixels, metadata.height, metadata.width);
 
-	draw(frame.pixels, frame.height, frame.width);
-
-	wl_surface_attach(state.wl_surface, frame.buffer, 0, 0);
-	wl_surface_damage_buffer(state.wl_surface, 0, 0, frame.width, frame.height);
+	wl_surface_attach(state.wl_surface, free_frame->buffer, 0, 0);
+	wl_surface_damage_buffer(state.wl_surface, 0, 0, metadata.width, metadata.height);
 	wl_surface_commit(state.wl_surface);
 }
 
 void xdg_toplevel_close(void *data, struct xdg_toplevel *xdg_toplevel)
 {
-	PRINT_LOG(LOG, "Event dispatched...");
+	PRINT_LOG(LOG, "Quitting...");
 
 	state.running = false;
 }
 
-void xdg_toplevel_configure_bounds (void *data, struct xdg_toplevel *xdg_toplevel, int32_t width, int32_t height)
-{
-	PRINT_LOG(LOG, "Event dispatched...");
-}
+void xdg_toplevel_configure_bounds(void *data, struct xdg_toplevel *xdg_toplevel, int32_t width, int32_t height) {}
+void xdg_toplevel_wm_capabilities(void *data, struct xdg_toplevel *xdg_toplevel, struct wl_array *capabilities) {}
 
-void xdg_toplevel_wm_capabilities (void *data, struct xdg_toplevel *xdg_toplevel, struct wl_array *capabilities)
-{
-	PRINT_LOG(LOG, "Event dispatched...");
-}
-
-void wl_buffer_release (void *data, struct wl_buffer *wl_buffer)
-{
-	PRINT_LOG(LOG, "Event dispatched...");
-
-	frame.free = true;
-	PRINT_LOG(LOG, "frame.free = %d", frame.free);
+void wl_buffer_release(void *data, struct wl_buffer *wl_buffer)
+{	
+	struct frame * frame_to_be_free = (struct frame *) data;
+	frame_to_be_free->free = true;
+	
+	PRINT_LOG(LOG, BOLD "frame[%d]" RESET " freed", frame_to_be_free->id);
 }
